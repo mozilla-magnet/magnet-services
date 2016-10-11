e43993ecd4709d2a0c14858b88138fb1d247d8b7
Sam Giles <sam.e.giles@gmail.com>
2016-10-11 16:55:21 +0100

    Make the URL match more precise

--- content-service/server/routes/api.js
+++ content-service/server/routes/api.js
@@ -56,7 +56,7 @@ router.get(/^\/v1\/channel\/(.*)\/beacons\/?$/, createRouteHandler((req, res) =>
 router.post(/^\/v1\/search\/beacons\/?$/, createRouteHandler((req, res) => {
 }));
 
-router.get(/^\/v1\/search\/shortid\/(.*)\/?$/, createRouteHandler((req, res) => {
+router.get(/^\/v1\/search\/shortid\/([a-zA-Z0-9_\-~]*)\/?$/, createRouteHandler((req, res) => {
   const shortId = req.params[0];
 
   return database.getCanonicalUrlForShortId(shortId)
