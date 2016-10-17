1476ffd1bd1ec3be6db5f8726929fc5155d54bc4
Sam Giles <sam.e.giles@gmail.com>
2016-10-17 13:56:57 +0100

    More organisation

--- content-service/server/database/index.js
+++ content-service/server/database/index.js
@@ -18,6 +18,7 @@ const knex = (function createPool() {
 const st = require('knex-postgis')(knex);
 const channels = require('./channels')(knex);
 const beacons = require('./beacons')(knex);
+const search = require('./search')(knex);
 
 function getAllBeaconsForChannel(channelName) {
   if (!(channelName && channelName.length)) {
@@ -34,69 +35,6 @@ function getAllBeaconsForChannel(channelName) {
     });
 }
 
-function searchBeacons(lat, long, radius) {
-
-  // ST_MakePoint is (x,y) so reverse conventional 'lat, long' to 'long, lat'
-  const point = st.makePoint(long, lat);
-
-  return knex('beacon')
-    .select('channel_name', 'id', st.asGeoJSON('location'))
-    .where(st.dwithin('location', point, radius))
-    .limit(100)
-    .then((dbResponse) => {
-      return dbResponse.map((entry) => {
-        const parsedGeoJson = JSON.parse(entry.location);
-        return {
-          slug: shortId.numToShortId(entry.id),
-          channel_name: entry.channel_name,
-          location: {
-            latitude: parsedGeoJson.coordinates[1],
-            longitude: parsedGeoJson.coordinates[0],
-          }
-        };
-      });
-    });
-}
-
-function searchSlugs(slugs) {
-
-  if (!Array.isArray(slugs)) {
-    throw new HttpError(400, 'Request body must be an array', 'EINVAL');
-  }
-
-  if (slugs.length > 100) {
-    throw new HttpError(400, 'Request body too large (limit of 100 items)', 'E2BIG');
-  }
-
-  if (slugs.length === 0) {
-    return Promise.resolve({});
-  }
-
-  const resultsToReturn = slugs.reduce((obj, slug) => {
-    obj[slug] = false;
-    return obj;
-  }, {});
-
-  return knex('beacon')
-    .select('channel_name', 'id', st.asGeoJSON('location'))
-    .whereIn('id', slugs.map(shortId.shortIdToNum))
-    .then((dbResponse) => {
-      dbResponse.forEach((entry) => {
-        const parsedGeoJson = JSON.parse(entry.location);
-        const slug = shortId.numToShortId(entry.id);
-        resultsToReturn[slug] = {
-          channel_name: entry.channel_name,
-          location: {
-            latitude: parsedGeoJson.coordinates[1],
-            longitude: parsedGeoJson.coordinates[0],
-          }
-        };
-      });
-
-      return resultsToReturn;
-    });
-};
-
 function getCanonicalUrlForShortId(id) {
   // SG:NOTE: Replace this with getBeaconInfo(id)
   return knex('beacon')
@@ -112,41 +50,14 @@ function getCanonicalUrlForShortId(id) {
     });
 }
 
-/*
- * Returns a geojson representation of every point in the database,
- * must be authenticated, might take a while!
- */
-function getAllPointsInDatabase() {
-  return knex('beacon')
-    .select('canonical_url', 'channel_name', st.asGeoJSON('location'))
-    .then((response) => {
-      return response.map((entry) => {
-        return {
-          type: 'Feature',
-          properties: {
-            url: entry.canonical_url,
-            channel: entry.channel_name
-          },
-          geometry: JSON.parse(entry.location)
-        };
-      });
-      return response;
-    })
-    .then((features) => {
-      return {
-        type: 'FeatureCollection',
-        features,
-      };
-    });
-}
 
 module.exports = {
   createNewBeacon: beacons.create,
   createNewChannel: channels.create,
   getAllBeaconsForChannel,
-  searchBeacons,
+  searchBeacons: search.beaconsByLocation,
   getCanonicalUrlForShortId,
-  searchSlugs,
-  getAllPointsInDatabase,
+  searchSlugs: search.slugs,
+  getAllPointsInDatabase: search.allPoints,
   getBeaconInfo: beacons.read,
 };
